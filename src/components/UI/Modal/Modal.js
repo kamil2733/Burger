9caf56991458d3c8f7c28ddb04ffc63cc8569096
kamil2733 <kamilmucha88@gmail.com>
2019-12-12 13:03:59 +0100

    fix hoc

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../hoc/Auxs';
 import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,7 @@
 import React, {
     Component
 } from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../components/hoc/Auxs';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
